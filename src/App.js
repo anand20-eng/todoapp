ada0b08fb8df10844178ee0fa4fad49bd0c411c9
anand20-eng <anand.gomaskar@gmail.com>
2024-12-05 14:34:23 +0530

    take navbar for card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 // import TodoList from './component/TodoList';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Create from './component/Create';
+import HeaderComp from './component/Card/Header'
 import {BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './component/HomeComponent'
 import UpdateComp from './component/updateComp';
@@ -15,7 +16,7 @@ function App() {
     <div>
        <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}> </Route>
+        <Route path="/" element={<HeaderComp />}> </Route>
         <Route path="/create" element={<Create />}> </Route>
         <Route path="/edit/:id" element={<UpdateComp />}> </Route>
 
