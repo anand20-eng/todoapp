5ab83706d30ef3000e1be5230e6c3e7d0101ab1f
anand20-eng <anand.gomaskar@gmail.com>
2024-12-10 15:04:18 +0530

    shop card done

--- src/component/Card/shopCard.js
+++ src/component/Card/shopCard.js
@@ -10,7 +10,7 @@ function ShopCard() {
           <div className="iteamsdetails">
             <div className="items_img">
               <img src="https://b.zmtcdn.com/data/pictures/9/18857339/8f53919f1175c08cf0f0371b73704f9b_o2_featured_v2.jpg?output-format=webp"
-                alt="" />
+                alt=""/>
             </div>
             <div className="details">
               <Table>
